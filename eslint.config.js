a9132b5df259456dfcdd50385157281a317b1077
cyrus <mdoaie@yahoo.co.uk>
2025-06-30 20:51:05 +0100

    Refactor code structure for improved readability and maintainability

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import config from '@pixpilot/dev-config/eslint';
 import jestConfig from '@pixpilot/dev-config/eslint-jest';
 
-/** @type {any[]} */
 const eslintConfig = [...config, ...jestConfig];
 
 export default eslintConfig;

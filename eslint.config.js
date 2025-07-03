277aff30d6e7069bf5c7f5d2111ee52071ae6230
cyrus <mdoaie@yahoo.co.uk>
2025-07-03 13:04:56 +0100

    chore: remove unused eslintignore file and update eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,17 @@
 import config from '@pixpilot/dev-config/eslint';
 import jestConfig from '@pixpilot/dev-config/eslint-jest';
 
-const eslintConfig = [...config, ...jestConfig];
+const eslintConfig = [
+  ...config,
+  ...jestConfig,
+  {
+    ignores: [
+      'src/types/global.d.ts',
+      'CHANGELOG.md',
+      ...config[0].ignores,
+      ...jestConfig[0].ignores,
+    ],
+  },
+];
 
 export default eslintConfig;

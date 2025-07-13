92f822e4eb26ee40569ed40b19fccf5e4a25cf82
ccpu <m.doaie@hotmail.com>
2025-07-13 13:10:08 +0100

    chore: update @pixpilot/dev-config to version 1.5.1 and adjust ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,19 @@
-import config from '@pixpilot/dev-config/eslint';
+import baseConfig from '@pixpilot/dev-config/eslint';
 import jestConfig from '@pixpilot/dev-config/eslint-jest';
+import prettierConfig from '@pixpilot/dev-config/eslint-prettier';
 
 const eslintConfig = [
-  ...config,
+  ...baseConfig,
   ...jestConfig,
   {
     ignores: [
       'src/types/global.d.ts',
       'CHANGELOG.md',
-      ...config[0].ignores,
+      ...baseConfig[0].ignores,
       ...jestConfig[0].ignores,
     ],
   },
+  ...prettierConfig,
 ];
 
 export default eslintConfig;

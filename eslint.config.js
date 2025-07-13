c9c5c536a6dd6bccb2922b3ce56c20456ce9e5b5
ccpu <m.doaie@hotmail.com>
2025-07-13 21:24:54 +0100

    chore: update ESLint configuration and dependencies to improve compatibility

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,14 @@
 import baseConfig from '@pixpilot/dev-config/eslint';
 import jestConfig from '@pixpilot/dev-config/eslint-jest';
 import prettierConfig from '@pixpilot/dev-config/eslint-prettier';
+import { includeIgnoreFile } from '@eslint/compat';
+import * as path from 'node:path';
 
-const eslintConfig = [...baseConfig, ...jestConfig, ...prettierConfig];
+const eslintConfig = [
+  includeIgnoreFile(path.join(import.meta.dirname, '.gitignore')),
+  ...baseConfig,
+  ...jestConfig,
+  ...prettierConfig,
+];
 
 export default eslintConfig;

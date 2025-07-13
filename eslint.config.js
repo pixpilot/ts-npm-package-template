f296004ef6db8475abc3403b8f882485608a348b
ccpu <m.doaie@hotmail.com>
2025-07-13 13:50:23 +0100

    chore: simplify ESLint configuration and update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -2,18 +2,6 @@ import baseConfig from '@pixpilot/dev-config/eslint';
 import jestConfig from '@pixpilot/dev-config/eslint-jest';
 import prettierConfig from '@pixpilot/dev-config/eslint-prettier';
 
-const eslintConfig = [
-  ...baseConfig,
-  ...jestConfig,
-  {
-    ignores: [
-      'src/types/global.d.ts',
-      'CHANGELOG.md',
-      ...baseConfig[0].ignores,
-      ...jestConfig[0].ignores,
-    ],
-  },
-  ...prettierConfig,
-];
+const eslintConfig = [...baseConfig, ...jestConfig, ...prettierConfig];
 
 export default eslintConfig;
